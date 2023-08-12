b34d907a92fcfca26050c47975fcdadba124aacf
Gabriel Jorge <gabriel.ggcj@gmail.com>
2023-08-11 22:18:05 -0300

    feature/add-global-style

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
+import { ThemeProvider } from "styled-components"
 import { Router } from "./Router"
 import { BrowserRouter } from 'react-router-dom'
+import { defaultTheme } from "./styles/themes/default"
+import { GlobalStyle } from "./styles/global"
 
 export const App = () => {
   return(
-    <BrowserRouter>
-      <Router/>
-    </BrowserRouter>
+    <ThemeProvider theme={defaultTheme}>
+      <BrowserRouter>
+        <Router/>
+      </BrowserRouter>
+      <GlobalStyle />
+    </ThemeProvider>
   )
 }
