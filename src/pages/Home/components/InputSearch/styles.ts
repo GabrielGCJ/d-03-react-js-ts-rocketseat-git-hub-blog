5baba1c4739c567df19c13de8cb3354004d2430a
gabrielj <gabriel.ggcj@gmail.com>
2023-08-18 00:41:56 -0300

    feature/add-update-input-search

--- src/pages/Home/components/InputSearch/styles.ts
+++ src/pages/Home/components/InputSearch/styles.ts
@@ -4,19 +4,23 @@ export const InputSearchContainer = styled.form`
   display: flex;
   align-items: center;
   justify-content: center;
-  width: 100%;
+`
+
+export const InputSearchContainerInternal = styled.div`
+  display: flex;
+  max-width: 864px;
   flex-direction: column;
+  width: 100%;
+  padding: 1rem;
 `
 export const InputAndButtonContainer = styled.div`
   display: flex;
-  flex-direction: row;
   justify-content: center;
   width: 100%;
-  padding: 1rem;
+  padding-top: 1rem;
 
   input {
     width: 100%;
-    max-width: 864px;
     height: 50px;
 
     background: ${(props) => props.theme['base-input']};
@@ -26,16 +30,12 @@ export const InputAndButtonContainer = styled.div`
 
     &::placeholder {
       color: ${(props) => props.theme['base-text']};
-      padding: 1rem;
+      padding-left: 1rem;
     }
   }
 `
 export const InformationsContainer = styled.div`
   display: flex;
-  flex-direction: row;
   justify-content: space-between;
   align-items: center;
-  width: 100%;
-  max-width: 864px;
-  padding: 1rem;
 `
