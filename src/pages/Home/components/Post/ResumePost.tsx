03105a5d48e3eddd75b0b7c02c1d452e851bf2bb
Gabriel Jorge <gabriel.ggcj@gmail.com>
2023-08-19 23:54:28 -0300

    feature/add-resume post

---
+++ src/pages/Home/components/Post/ResumePost.tsx
@@ -0,0 +1,32 @@
+import {
+  PostContainer,
+  PublicationBody,
+  TimerContainer,
+  TitleAndTimerContainer,
+  TitleContainer,
+} from './styles'
+
+export const ResumePost = () => {
+  return (
+    <PostContainer>
+      <TitleAndTimerContainer>
+        <TitleContainer>
+          <p>JavaScript data types and data structures</p>
+        </TitleContainer>
+        <TimerContainer>
+          <p>Há um dia</p>
+        </TimerContainer>
+      </TitleAndTimerContainer>
+      {/* <h1>Post</h1> */}
+      <PublicationBody>
+        <p>
+          Programming languages all have built-in data structures, but these
+          often differ from one language to another. This article attempts to
+          list the built-in data structures available in JavaScript and what
+          properties they have. These can be used to build other data
+          structures.{' '}
+        </p>
+      </PublicationBody>
+    </PostContainer>
+  )
+}
---
+++ src/pages/Home/components/Post/styles.ts
@@ -0,0 +1,57 @@
+import styled from 'styled-components'
+
+export const PostContainer = styled.div`
+  display: flex;
+  max-width: 416px;
+  width: 100%;
+  height: 260px;
+  /* flex-shrink: 0; // Pesquisar sobre ! */
+  border-radius: 10px;
+  background-color: ${(props) => props.theme['base-post']};
+
+  flex-direction: column;
+
+  justify-content: center;
+  align-items: center;
+  padding: 30px;
+`
+export const TitleAndTimerContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  /* align-items:center ; */
+
+  gap: 1 rem;
+`
+export const TitleContainer = styled.div`
+  display: flex;
+  font-family: Nunito;
+  font-size: 20px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 160%; /* 32px */
+
+  padding: 0 20px 20px 0px;
+`
+export const TimerContainer = styled.div`
+  font-family: Nunito;
+  font-size: 14px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 160%; /* 22.4px */
+
+  /* background: red; */
+
+  min-width: 70px;
+`
+export const PublicationBody = styled.div`
+  overflow: hidden;
+  color: var(--base-text, #afc2d4);
+  text-overflow: ellipsis;
+  /* white-space: nowrap; */
+
+  font-family: Nunito;
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 160%; 25.6px
+`
