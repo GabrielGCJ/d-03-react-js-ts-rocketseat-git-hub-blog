a15fa0ad8806058c730a757070c5b867da53e828
Gabriel Jorge <gabriel.ggcj@gmail.com>
2023-08-25 05:40:36 -0300

    feature/add-blog-context

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -2,12 +2,8 @@ import { InputSearch } from './components/InputSearch/InputSearch'
 import { Profile } from './components/Profile/Profile'
 import { AllResumePosts } from './components/AllResumePosts/AllResumePosts'
 import { HomeContainer } from './styles'
-import { useContext } from 'react'
-import { BlogContext } from '../../contexts/BlogContext'
 
 export const Home = () => {
-  const { infoBlog } = useContext(BlogContext)
-  console.log(infoBlog)
   return (
     <HomeContainer>
       <Profile />
