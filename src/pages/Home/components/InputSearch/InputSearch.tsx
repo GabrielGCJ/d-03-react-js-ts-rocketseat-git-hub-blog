610c8e0bf3c109e7add96c74d18881f9b477271e
gabrielj <gabriel.ggcj@gmail.com>
2023-08-29 06:24:19 -0300

    feature/add-new-updates

--- src/pages/Home/components/InputSearch/InputSearch.tsx
+++ src/pages/Home/components/InputSearch/InputSearch.tsx
@@ -1,17 +1,25 @@
+import { useContext } from 'react'
 import {
   InformationsContainer,
   InputAndButtonContainer,
   InputSearchContainer,
   InputSearchContainerInternal,
 } from './styles'
+import { BlogContext } from '../../../../contexts/BlogContext'
 
 export const InputSearch = () => {
+  const { issues } = useContext(BlogContext)
   return (
     <InputSearchContainer>
       <InputSearchContainerInternal>
         <InformationsContainer>
           <h2>Publicações</h2>
-          <p>6 publicações</p>
+
+          {issues.length >= 2 ? (
+            <p>{issues.length} publicações</p>
+          ) : (
+            <p>{issues.length} publicação</p>
+          )}
         </InformationsContainer>
         <InputAndButtonContainer>
           <input placeholder="Buscar conteúdo" />
